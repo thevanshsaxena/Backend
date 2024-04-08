1e8adad13b4c46a906ac7848b95d4b4f70c0b379
thevanshsaxena <151606139+thevanshsaxena@users.noreply.github.com>
2024-04-08 12:52:35 -0700

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import likeRoutes from "./routes/likes.js";
+import cookieParser from "cookie-parser";
+
+
+const app = express();
+//middlewares
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/likes", likeRoutes);
+
+app.listen(8800, () => {
+  console.log("API working!");
+});
